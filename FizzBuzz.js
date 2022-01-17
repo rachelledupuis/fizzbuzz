ebac28451555309ac8b0fb7f29548b87d42cc824
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-17 19:41:35 +0000

    Replaced if/else statements with ternary operator, created variables for fizz and buzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,8 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0){
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
-    }
+    let fizz = 'Fizz';
+    let buzz = 'Buzz';
+    
+    i % 3 === 0 && i % 5 === 0 ? console.log(fizz + buzz)
+    : i % 3 === 0 ? console.log(fizz)
+    : i % 5 === 0 ? console.log(buzz) : console.log(i);
 };
