ed170ff4d04f6e264127119589de3559ad8c84a2
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-18 11:21:04 +0000

    Completed stretch goal #1: check if multiple of 7, print 'Bang'

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -6,10 +6,13 @@ for (let i = 1; i <= 100; i++) {
     let result = '';
 
     if (isDivisibleBy(i, 3)) {
-        result = 'Fizz';
+        result += 'Fizz';
     }
     if (isDivisibleBy(i, 5)) {
         result += 'Buzz';
+    }
+    if(isDivisibleBy(i, 7)) {
+        result += 'Bang';
     }
     if (result === '') {
         result = i;
